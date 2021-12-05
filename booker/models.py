d678f9226ad7a212f298fdca694cb45d5304aee5
Jekayinoluwa Olabemiwo <jkaylight@gmail.com>
2021-12-05 13:55:29 +0100

    created migrations

--- booker/models.py
+++ booker/models.py
@@ -23,7 +23,7 @@ class Doctor(models.Model):
         ('2pm', '2pm - 3pm'),
         ('3pm', '3pm - 4pm'),
     ]
-    time_available = models.CharField(max_length=2, choices=TIME_AVAILABLE_CHOICES)
+    time_available = models.CharField(max_length=4, choices=TIME_AVAILABLE_CHOICES)
     
     def __str__(self):
         return self.name
@@ -32,7 +32,7 @@ class Doctor(models.Model):
 class Appointment(models.Model):
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
     schedule_date = models.DateField()
-    time_selected = models.charField(max_length=255)
+    time_selected = models.CharField(max_length=255)
 
     # TIME_AVAILABLE_CHOICES = [
     #     ('9am', '9am - 10am'),
--- booker/urls.py
+++ booker/urls.py
@@ -1,9 +1,9 @@
 # created this new ULS file
 
 from django.urls import path
-from booker import views
+from booker import views # added new line
 
 urlpatterns = [
     path('speciality/', views.SpecialityList),
-    path('appointments/', views.AppointmentsList),
+    # path('appointments/', views.AppointmentList),
 ]
