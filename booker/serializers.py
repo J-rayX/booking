412dd659ddbf775f6cc5ad1581e0d61d786b0932
Jekayinoluwa Olabemiwo <jkaylight@gmail.com>
2021-12-11 12:08:57 +0100

    change specialitySerialize to show doctors details

--- booker/serializers.py
+++ booker/serializers.py
@@ -1,19 +1,22 @@
 from rest_framework import serializers
 from .models import Doctor, Speciality, Appointment
 
-class SpecialitySerializer(serializers.ModelSerializer):
-    doctors = serializers.PrimaryKeyRelatedField(many=True, read_only=True) 
-    class Meta:
-        model = Speciality
-        fields = ['id', 'title', 'doctors']
-        # multiplechoices = serializers.MultipleChoiceField(choices = TIME_AVAILABLE_CHOICES)
-
 
 class DoctorSerializer(serializers.ModelSerializer):
     speciality = serializers.PrimaryKeyRelatedField(queryset=Speciality.objects.all(), many=False)
     class Meta:
         model = Doctor
         fields = ['id', 'name', 'email', 'speciality']
+
+
+class SpecialitySerializer(serializers.ModelSerializer):
+    # doctors = serializers.PrimaryKeyRelatedField(many=True, read_only=True) 
+    doctors = DoctorSerializer(many=True)
+    class Meta:
+        model = Speciality
+        # fields = ['id', 'title', 'doctors']
+        fields = '__all__'
+        # multiplechoices = serializers.MultipleChoiceField(choices = TIME_AVAILABLE_CHOICES)
 
 
 
