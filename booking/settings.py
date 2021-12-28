d90dd0e256275e8b0f7df6a63a140c45f84f5cbe
Jekayinoluwa Olabemiwo <jkaylight@gmail.com>
2021-12-28 01:33:37 +0100

    added CORS configuration

--- booking/settings.py
+++ booking/settings.py
@@ -38,12 +38,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework', # added new line
+    'corsheaders', # add this line after the above before the below
     'booker', # added new line
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8080',  # for localhost Vue app
+)
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
